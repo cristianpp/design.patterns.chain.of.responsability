08032967a9618d9c35004f9159edd653ad531c09
cristianpietro <cristian.pietro@gmail.com>
2021-04-18 10:06:10 -0300

    Adding the template method

--- src/com/br/designpatterns/chain/of/responsability/SemDesconto.java
+++ src/com/br/designpatterns/chain/of/responsability/SemDesconto.java
@@ -7,9 +7,15 @@ public class SemDesconto extends Desconto{
 	public SemDesconto(){
 		super(null);
 	}
-	
-	public BigDecimal calcular(Orcamento orcamento ){
+
+	@Override
+	protected BigDecimal efetuarCalculo(Orcamento orcamento) {
 		return BigDecimal.ZERO;
+	}
+
+	@Override
+	protected boolean deveAplicar(Orcamento orcamento) {
+		return true;
 	}
 	
 
